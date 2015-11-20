ef7123c70de7dc06eea73e542dba3a4eeb42c57a
Mario Blazek <mario.b@netgen.hr>
2015-11-20 21:55:14 +0100

    Implemented ezbinaryfile form handler

---
+++ Form/FieldTypeHandler/File.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Netgen\Bundle\EzFormsBundle\Form\FieldTypeHandler;
+
+use Netgen\Bundle\EzFormsBundle\Form\FieldTypeHandler;
+use Symfony\Component\Form\FormBuilderInterface;
+use eZ\Publish\API\Repository\Values\Content\Content;
+use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
+use eZ\Publish\SPI\FieldType\Value;
+use Symfony\Component\Validator\Constraints;
+
+/**
+ * Class File
+ *
+ * @package Netgen\EzFormsBundle\FieldType\FormBuilder
+ */
+class File extends FieldTypeHandler
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @param \eZ\Publish\Core\FieldType\Image\Value $value
+     */
+    public function convertFieldValueToForm( Value $value )
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param null|\Symfony\Component\HttpFoundation\File\UploadedFile $data
+     */
+    public function convertFieldValueFromForm( $data )
+    {
+        if ( $data === null )
+        {
+            return null;
+        }
+
+        return array(
+            "inputUri" => $data->getRealPath(),
+            "fileName" => $data->getClientOriginalName(),
+            "fileSize" => $data->getSize(),
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function buildFieldForm(
+        FormBuilderInterface $formBuilder,
+        FieldDefinition $fieldDefinition,
+        $languageCode,
+        Content $content = null
+    )
+    {
+        $options = $this->getDefaultFieldOptions( $fieldDefinition, $languageCode, $content );
+        $maxFileSize = $fieldDefinition->validatorConfiguration["FileSizeValidator"]["maxFileSize"];
+
+        if ( $maxFileSize !== false )
+        {
+            $options["constraints"][] = new Constraints\File(
+                array(
+                    "maxSize" => $maxFileSize * Constraints\FileValidator::MB_BYTES,
+                )
+            );
+        }
+
+        // File should not be erased (updated as empty) if nothing is selected in file input
+        $this->skipEmptyUpdate( $formBuilder, $fieldDefinition->identifier );
+        // Used with update for displaying current file
+        $options["block_name"] = "ezforms_image";
+
+        $formBuilder->add( $fieldDefinition->identifier, "file", $options );
+    }
+}
