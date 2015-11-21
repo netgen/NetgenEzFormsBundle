9d9e649dd029435ec80ef07ec5944761acc76948
Mario Blazek <mario.b@netgen.hr>
2015-11-21 16:47:07 +0100

    FieldTypeHandlers return Value object not scalars or arrays

--- Form/FieldTypeHandler/TextBlock.php
+++ Form/FieldTypeHandler/TextBlock.php
@@ -6,6 +6,7 @@ use Netgen\Bundle\EzFormsBundle\Form\FieldTypeHandler;
 use Symfony\Component\Form\FormBuilderInterface;
 use eZ\Publish\API\Repository\Values\Content\Content;
 use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
+use eZ\Publish\Core\FieldType\TextBlock\Value as TextBlockValue;
 use eZ\Publish\SPI\FieldType\Value;
 
 /**
@@ -23,6 +24,19 @@ class TextBlock extends FieldTypeHandler
     public function convertFieldValueToForm( Value $value )
     {
         return $value->text;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertFieldValueFromForm( $data )
+    {
+        if ( empty( $data ) )
+        {
+            $data = '';
+        }
+
+        return new TextBlockValue( $data );
     }
 
     /**
