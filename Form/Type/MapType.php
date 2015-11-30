515e166c07e72fd63aa6a52f17b5219a3d639389
Mario Blazek <mario.b@netgen.hr>
2015-11-30 10:56:54 +0100

    Moved hidden fields and buttons to presentation layer

--- Form/Type/MapType.php
+++ Form/Type/MapType.php
@@ -20,42 +20,12 @@ class MapType extends AbstractType
             "label" => "ezforms.form.map.address.label",
         ));
 
-        $builder->add("address_hidden", "hidden", array(
-            "label" => "ezforms.form.map.address.label",
-        ));
-
-        $builder->add("find_address", "button", array(
-            "label" => "ezforms.form.map.find_address.label",
-        ));
-
-        $builder->add("restore_coordinates", "button", array(
-            "label" => "ezforms.form.map.restore_coordinates.label",
-            "attr" => array(
-                "disabled" => true,
-            ),
-        ));
-
         $builder->add("latitude", "number", array(
-            "label" => "ezforms.form.map.latitude.label",
-        ));
-
-        $builder->add("latitude_hidden", "hidden", array(
             "label" => "ezforms.form.map.latitude.label",
         ));
 
         $builder->add("longitude", "number", array(
             "label" => "ezforms.form.map.longitude.label",
-        ));
-
-        $builder->add("longitude_hidden", "hidden", array(
-            "label" => "ezforms.form.map.longitude.label",
-        ));
-
-        $builder->add("current_location", "button", array(
-            "label" => "ezforms.form.map.current_location.label",
-            "attr" => array(
-                "class" => "ez-gmap-my-location-button",
-            ),
         ));
     }
 
