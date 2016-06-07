25959f362d71aa5093d55349850287f53c741703
Mario Blazek <mario.b@netgen.hr>
2016-06-07 10:17:50 +0200

    CreateUserType and UpdateUserType tests

--- Tests/Form/Type/UpdateUserTypeTest.php
+++ Tests/Form/Type/UpdateUserTypeTest.php
@@ -3,8 +3,14 @@
 namespace Netgen\Bundle\EzFormsBundle\Tests\Form\Type;
 
 use eZ\Publish\API\Repository\Values\User\UserUpdateStruct;
+use eZ\Publish\Core\Repository\Values\Content\Content;
+use eZ\Publish\Core\Repository\Values\Content\ContentUpdateStruct;
+use eZ\Publish\Core\Repository\Values\Content\VersionInfo;
 use eZ\Publish\Core\Repository\Values\ContentType\ContentType;
+use eZ\Publish\Core\Repository\Values\ContentType\FieldDefinition;
+use eZ\Publish\SPI\Persistence\Content\ContentInfo;
 use Netgen\Bundle\EzFormsBundle\Form\DataWrapper;
+use Netgen\Bundle\EzFormsBundle\Form\FieldTypeHandler;
 use Netgen\Bundle\EzFormsBundle\Form\FieldTypeHandlerRegistry;
 use Netgen\Bundle\EzFormsBundle\Form\Type\UpdateUserType;
 use Symfony\Component\Form\AbstractType;
@@ -137,6 +143,112 @@ class UpdateUserTypeTest extends \PHPUnit_Framework_TestCase
         $user = new User();
 
         $options = array('data' => new DataWrapper($userUpdateStruct, null, $user));
+
+        $updateUserType = new UpdateUserType($handlerRegistry, $dataMapper);
+        $updateUserType->buildForm($formBuilder, $options);
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Data definition (ContentType) does not correspond to the data target (Content)
+     */
+    public function testBuildFormContentTypeIdsMustMatch()
+    {
+        $handlerRegistry = $this->getMockBuilder(FieldTypeHandlerRegistry::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array())
+            ->getMock();
+
+        $dataMapper = $this->getMockForAbstractClass(DataMapperInterface::class);
+
+        $formBuilder = $this->getMockBuilder(FormBuilder::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array('setDataMapper'))
+            ->getMock();
+
+        $contentInfo = new ContentInfo(array('contentTypeId' => 123));
+        $versionInfo = new VersionInfo(array('contentInfo' => $contentInfo));
+        $content = new Content(array('versionInfo' => $versionInfo));
+        $user = new User(array('content' => $content));
+
+        $userUpdateStruct = new UserUpdateStruct();
+
+        $contentType = new ContentType(
+            array(
+                'id' => 654,
+                'fieldDefinitions' => array(
+                    new FieldDefinition(
+                        array(
+                            'id' => 'id',
+                            'identifier' => 'identifier',
+                        )
+                    ),
+                )
+            )
+        );
+
+        $options = array('data' => new DataWrapper($userUpdateStruct, $contentType, $user));
+
+        $updateUserType = new UpdateUserType($handlerRegistry, $dataMapper);
+        $updateUserType->buildForm($formBuilder, $options);
+    }
+
+
+    public function testBuildForm()
+    {
+        $fieldTypeHandler = $this->getMockBuilder(FieldTypeHandler::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array('buildFieldUpdateForm'))
+            ->getMockForAbstractClass();
+
+        $fieldTypeHandler->expects($this->once())
+            ->method('buildFieldUpdateForm');
+
+
+        $handlerRegistry = $this->getMockBuilder(FieldTypeHandlerRegistry::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array('get'))
+            ->getMock();
+
+        $handlerRegistry->expects($this->once())
+            ->willReturn($fieldTypeHandler)
+            ->method('get');
+
+        $dataMapper = $this->getMockForAbstractClass(DataMapperInterface::class);
+
+        $formBuilder = $this->getMockBuilder(FormBuilder::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array('setDataMapper'))
+            ->getMock();
+
+
+        $contentInfo = new ContentInfo(array('contentTypeId' => 123));
+        $versionInfo = new VersionInfo(array('contentInfo' => $contentInfo));
+        $content = new Content(array('versionInfo' => $versionInfo));
+        $user = new User(array('content' => $content));
+
+        $contentUpdateStruct = new ContentUpdateStruct(
+            array(
+                'initialLanguageCode' => 'eng-GB',
+            )
+        );
+        $userUpdateStruct = new UserUpdateStruct(array('contentUpdateStruct' => $contentUpdateStruct));
+
+        $contentType = new ContentType(
+            array(
+                'id' => 123,
+                'fieldDefinitions' => array(
+                    new FieldDefinition(
+                        array(
+                            'id' => 'id',
+                            'identifier' => 'identifier',
+                        )
+                    ),
+                )
+            )
+        );
+
+        $options = array('data' => new DataWrapper($userUpdateStruct, $contentType, $user));
 
         $updateUserType = new UpdateUserType($handlerRegistry, $dataMapper);
         $updateUserType->buildForm($formBuilder, $options);
