f05715ce15a074a2080aca1eb90cfaf83c8a7ac4
Mario Blazek <mario.b@netgen.hr>
2016-06-08 14:33:43 +0200

    Date and DateAndTime handlers test

---
+++ Tests/Form/FieldTypeHandler/DateAndTimeTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Netgen\Bundle\EzFormsBundle\Tests\Form\FieldTypeHandler;
+
+use eZ\Publish\Core\Repository\Values\ContentType\FieldDefinition;
+use Netgen\Bundle\EzFormsBundle\Form\FieldTypeHandler;
+use Netgen\Bundle\EzFormsBundle\Form\FieldTypeHandler\DateAndTime;
+use eZ\Publish\Core\FieldType\DateAndTime as DTValue;
+use Symfony\Component\Form\FormBuilder;
+
+class DateAndTimeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAssertInstanceOfFieldTypeHandler()
+    {
+        $dateAndTime = new DateAndTime();
+
+        $this->assertInstanceOf(FieldTypeHandler::class, $dateAndTime);
+    }
+
+    public function testConvertFieldValueToForm()
+    {
+        $dateAndTime = new DateAndTime();
+        $dateTime = new \DateTime();
+        $dateValue = new DTValue\Value($dateTime);
+
+        $returnedDate = $dateAndTime->convertFieldValueToForm($dateValue);
+
+        $this->assertEquals($dateTime, $returnedDate);
+    }
+
+    public function testConvertFieldValueFromForm()
+    {
+        $dateTime = new \DateTime();
+        $dateAndTime = new DateAndTime();
+        $dateValue = new DTValue\Value($dateTime);
+
+        $returnedDate = $dateAndTime->convertFieldValueFromForm($dateTime);
+
+        $this->assertEquals($dateValue, $returnedDate);
+    }
+
+    public function testBuildFieldCreateForm()
+    {
+        $formBuilder = $this->getMockBuilder(FormBuilder::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array('add'))
+            ->getMock();
+
+        $formBuilder->expects($this->once())
+            ->method('add');
+
+        $fieldDefinition = new FieldDefinition(
+            array(
+                'id' => 'id',
+                'identifier' => 'identifier',
+                'isRequired' => true,
+                'descriptions' => array('fre-FR' => 'fre-FR'),
+                'names' => array('fre-FR' => 'fre-FR'),
+            )
+        );
+
+        $languageCode = 'eng-GB';
+
+        $dateAndTime = new DateAndTime();
+
+        $dateAndTime->buildFieldCreateForm($formBuilder, $fieldDefinition, $languageCode);
+    }
+}
---
+++ Tests/Form/FieldTypeHandler/DateTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Netgen\Bundle\EzFormsBundle\Tests\Form\FieldTypeHandler;
+
+use eZ\Publish\Core\Repository\Values\ContentType\FieldDefinition;
+use Netgen\Bundle\EzFormsBundle\Form\FieldTypeHandler;
+use Netgen\Bundle\EzFormsBundle\Form\FieldTypeHandler\Date;
+use eZ\Publish\Core\FieldType\Date as DateValue;
+use Symfony\Component\Form\FormBuilder;
+
+class DateTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAssertInstanceOfFieldTypeHandler()
+    {
+        $date = new Date();
+
+        $this->assertInstanceOf(FieldTypeHandler::class, $date);
+    }
+
+    public function testConvertFieldValueToForm()
+    {
+        $dateTime = new \DateTime();
+        $date = new Date();
+        $dateValue = new DateValue\Value($dateTime);
+
+        $returnedDate = $date->convertFieldValueToForm($dateValue);
+        $dateTime->setTime( 0, 0, 0 );
+        $this->assertEquals($dateTime, $returnedDate);
+    }
+
+    public function testConvertFieldValueFromForm()
+    {
+        $dateTime = new \DateTime();
+        $date = new Date();
+        $dateValue = new DateValue\Value($dateTime);
+
+        $returnedDate = $date->convertFieldValueFromForm($dateTime);
+
+        $this->assertEquals($dateValue, $returnedDate);
+    }
+
+    public function testBuildFieldCreateForm()
+    {
+        $formBuilder = $this->getMockBuilder(FormBuilder::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array('add'))
+            ->getMock();
+
+        $formBuilder->expects($this->once())
+            ->method('add');
+
+        $fieldDefinition = new FieldDefinition(
+            array(
+                'id' => 'id',
+                'identifier' => 'identifier',
+                'isRequired' => true,
+                'descriptions' => array('fre-FR' => 'fre-FR'),
+                'names' => array('fre-FR' => 'fre-FR'),
+            )
+        );
+
+        $languageCode = 'eng-GB';
+
+        $date = new Date();
+
+        $date->buildFieldCreateForm($formBuilder, $fieldDefinition, $languageCode);
+    }
+}
